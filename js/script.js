fc3fc1a927a672cb86150f9c7f62fffa6b4dc849
sejnicho <sejnicho@ucsc.edu>
2024-07-31 16:05:05 -0700

    messin with some js

--- js/script.js
+++ js/script.js
@@ -2,4 +2,26 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-link').addEventListener('click', () => {
       document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
     })
-  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach( el => {
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+
+});
